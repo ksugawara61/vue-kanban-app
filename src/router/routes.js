93059124db4cf65cbde16c66fb75d73fb8824510
ksugawara61 <katsuya@kokohadoko.net>
2019-01-20 00:12:40 +0900

    [add] KbnLoginView and [fix] router indent

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,17 +3,17 @@ import KbnLoginView from '@/components/templates/KbnLoginView.vue'
 import KbnTaskDetailModal from '@/components/templates/KbnTaskDetailModal.vue'
 
 export default [{
-    path: '/',
-    component: KbnBoardView,
-    meta: { requiresAuth: true }
+  path: '/',
+  component: KbnBoardView,
+  meta: { requiresAuth: true }
 }, {
-    path: '/login',
-    component: KbnLoginView
+  path: '/login',
+  component: KbnLoginView
 }, {
-    path: '/task/:id',
-    component: KbnTaskDetailModal,
-    meta: { requiresAuth: true }
+  path: '/task/:id',
+  component: KbnTaskDetailModal,
+  meta: { requiresAuth: true }
 }, {
-    path: '*',
-    redirect: '/'
+  path: '*',
+  redirect: '/'
 }]
